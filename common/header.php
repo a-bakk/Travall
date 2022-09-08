6fd711b803ab06fd0585106b0c726f240341fc78
abakk <abelbakk@pm.me>
2022-09-08 22:57:01 +0200

    updated basics

---
+++ common/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <title>Főoldal</title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="assets/img/ICON"/> <!-- replace icon -->
+</head>
+<body>
+    <main>
+        <h1>Lorem Ipsum</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </main>
+</body>
+</html>
