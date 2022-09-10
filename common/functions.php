5fa2822cd7cce479ee136b13383ecfb286f9dafa
abakk <abelbakk@pm.me>
2022-09-10 14:58:02 +0200

    further styling

--- common/functions.php
+++ common/functions.php
@@ -12,10 +12,19 @@ function navGen() {
         "<a href='index.php'" . ($tmp === "index" ? " class='active'" : "") . ">Főoldal</a>" .
     "</li>" .
     "<li class = 'navelement'>" .
-        "<a href='contact.php'" . ($tmp === "contact" ? " class='active'" : "") . ">Kapcsolat</a>" .
+        "<a href='flights.php'" . ($tmp === "flights" ? " class='active'" : "") . ">Járatok</a>" .
     "</li>" .
     "<li class = 'navelement'>" .
-        "<a href='third.php'" . ($tmp === "third" ? " class='active'" : "") . ">Third</a>" .
+        "<a href='cities.php'" . ($tmp === "cities" ? " class='active'" : "") . ">Városok</a>" .
+    "</li>" .
+    "<li class = 'navelement'>" .
+        "<a href='booking.php'" . ($tmp === "booking" ? " class='active'" : "") . ">Foglalás</a>" .
+    "</li>" .
+    "<li class = 'navelement'>" .
+        "<a href='clients.php'" . ($tmp === "clients" ? " class='active'" : "") . ">Ügyfelek</a>" . // admin priv required
+    "</li>" .
+    "<li class = 'navelement'>" .
+        "<a href='contact.php'" . ($tmp === "contact" ? " class='active'" : "") . ">Kapcsolat</a>" .
     "</li>";
 
 }
